0bb13db14f6eff1666b10270e9d313fa7a7e24ec
Konstantin <kfarr2@pdx.edu>
2015-12-08 22:45:14 -0800

    First Commit

---
+++ htdocs/index.php
@@ -0,0 +1,56 @@
+<?php
+    $title = "Where's the mouse?";
+    include('lib/header.php');
+?>
+<body>
+    <div id="origin" class="text-muted"><span class="glyphicon glyphicon-info-sign"></span></div>
+    <div id="thing-script">
+    <script>
+	// Refresh this "well" div,
+	// and the coordinates shown above.
+	function refreshValues(){
+            var thingCode = $("#thing-script").html();
+            var thingHook = $("#thing-hook").html();
+
+            $("#thing-hook").html(thingHook);
+            $("#thing-script").html(thingCode);
+            $("#code").text(thingCode + thingHook);
+	}
+
+	// Get current mouse coordinates.
+	// Called every mouse movement.
+        function getCoordinates(event){
+            var x = event.clientX;
+            var y = event.clientY;
+            var coordinates = "X: " + x + "<br />Y: " + y;
+
+            $("#thing").html(coordinates);
+            refreshValues();
+	}
+    </script>
+    </div>
+    <div id="main-container" onmousemove="getCoordinates(event)">
+        <div class="container">
+            <div class="text-center">
+                <h1>Where's The Mouse?</h1>
+                <div id="thing-hook">
+    <h4 class="text-success" id="thing">MOVE THE MOUSE<br />do it</h4>
+                </div>
+                <h3>Want that code? Get that code</h3>
+            </div>
+            <pre id="code"></pre>
+            <br />
+            <script type="text/javascript" src="http://konstantinfarrell.github.io/static/js/credit.js"></script>
+        </div>
+    </div>
+    <script>
+	refreshValues();
+        $("#origin").click(function(){
+            $("#origin").toggle("fade").promise().done(function(){
+                $("#origin").html("<span class='glyphicon glyphicon-info-sign'></span> (0,0) <span class='glyphicon glyphicon-arrow-left'></span> Remember, the origin is here.");
+                $("#origin").css("opacity", 1.0);
+                $("#origin").toggle("fade");
+            });
+        });
+    </script>
+</body>
---
+++ htdocs/lib/header.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <script src="//cdn.research.pdx.edu/jquery/2.1.1/jquery-2.1.1.min.js"></script>
+    <script src="//cdn.research.pdx.edu/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script src="//cdn.research.pdx.edu/jqueryui/1.11.4/jquery-ui.min.js"></script>
+    <link rel="stylesheet" href="//cdn.research.pdx.edu/jqueryui/1.11.4/jquery-ui.min.css">
+    <link rel="stylesheet" href="//cdn.research.pdx.edu/bootstrap/3.3.5/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="main.css" />
+    <title><?php echo($title); ?></title>
+</head>
