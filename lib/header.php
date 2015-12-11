1851575a15895fb566e3f1859bd4017118bcc751
Konstantin <kfarr2@pdx.edu>
2015-12-11 09:27:09 -0800

    update favicon link

--- lib/header.php
+++ lib/header.php
@@ -4,9 +4,9 @@
     <script src="//cdn.research.pdx.edu/jquery/2.1.1/jquery-2.1.1.min.js"></script>
     <script src="//cdn.research.pdx.edu/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <script src="//cdn.research.pdx.edu/jqueryui/1.11.4/jquery-ui.min.js"></script>
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <link rel="stylesheet" href="//cdn.research.pdx.edu/jqueryui/1.11.4/jquery-ui.min.css">
     <link rel="stylesheet" href="//cdn.research.pdx.edu/bootstrap/3.3.5/css/bootstrap.min.css" />
     <link rel="stylesheet" href="main.css" />
     <title><?php echo($title); ?></title>
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
 </head>
